60bf15e87e095037af45cf5f2fc3f58d94514461
barletjb <barletjb.perso@gmail.com>
2025-10-06 23:00:24 +0200

    Add logger | DBAppender & FILEAppender

--- src/main/java/com/example/aiapp/service/OpenRouterService.java
+++ src/main/java/com/example/aiapp/service/OpenRouterService.java
@@ -1,28 +1,32 @@
 package com.example.aiapp.service;
 
 import com.example.aiapp.dal.OpenRouterDal;
-import com.example.aiapp.repository.AiLogRepository;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 
 @Service
-@Slf4j
 public class OpenRouterService {
 
-    private final AiLogRepository aiLogRepository;
     private final OpenRouterDal openRouterDal;
+    private static final Logger logger = LoggerFactory.getLogger(OpenRouterService.class);
 
-    public OpenRouterService(OpenRouterDal openRouterDal, AiLogRepository aiLogRepository) {
+    public OpenRouterService(OpenRouterDal openRouterDal) {
         this.openRouterDal = openRouterDal;
-        this.aiLogRepository = aiLogRepository;
     }
 
     public String generateResponse(String prompt){
 
-        log.info("Prompt: " + prompt);
+        String response = "";
+        try {
+            response = openRouterDal.sendPrompt(prompt);
+            logger.info("Message INFO : Prompt: " + prompt);
+            logger.warn("Message WARN : Prompt: " + prompt);
 
-        String response = openRouterDal.sendPrompt(prompt);
+        }catch (Exception e){
+            logger.error("Message ERROR : Prompt: " + prompt);
+        }
 
         return response;
     }
